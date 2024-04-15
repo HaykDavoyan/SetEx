e39eab2e3ec66849cd8a21334cddf51412f1968d
Hayk Davoyan <hayk.davoyan.2004@gmail.com>
2024-04-15 19:25:37 +0400

    Add project files.

---
+++ SetEx/MySet.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+namespace SetEx;
+
+public class MySet<T> : IEnumerable<T>
+    where T : IComparable<T>
+{
+    private readonly List<T> _items = new List<T>();
+
+    public MySet()
+    {
+
+    }
+    public MySet(IEnumerable<T> items)
+    {
+        AddRange(items);
+    }
+
+    public void Add(T item)
+    {
+        if (Contains(item))
+        {
+            throw new InvalidOperationException("Item already exists in Set");
+        }
+        _items.Add(item);
+    }
+    public void AddRange(IEnumerable<T> items)
+    {
+        foreach (T item in items)
+        {
+            Add(item);
+        }
+    }
+    public bool Remove(T item)
+    {
+        return _items.Remove(item);
+    }
+    public bool Contains(T item)
+    {
+        return (_items.Contains(item));
+    }
+    public int Count
+    {
+        get
+        {
+            return _items.Count;
+        }
+    }
+    public MySet<T> Union(MySet<T> other)
+    {
+        var union = new MySet<T>(_items);
+        foreach (T item in other)
+        {
+            if (!union.Contains(item))
+                union.Add(item);
+        }
+        return union;
+    }
+
+    public MySet<T> Intersection(MySet<T> other)
+    {
+        var intersection = new MySet<T>();
+        foreach (T item in _items)
+        {
+            if (other.Contains(item))
+                intersection.Add(item);
+        }
+        return intersection;
+    }
+
+    public MySet<T> Difference(MySet<T> other)
+    {
+        var difference = new MySet<T>(_items);
+        foreach (T item in other)
+        {
+            difference.Remove(item);
+        }
+        return difference;
+    }
+
+    public MySet<T> SymmetricDifference(MySet<T> other)
+    {
+        var symmetricDifference = new MySet<T>();
+        foreach (T item in _items)
+        {
+            if (!other.Contains(item))
+                symmetricDifference.Add(item);
+        }
+
+        foreach (T item in other)
+        {
+            if (!_items.Contains(item))
+                symmetricDifference.Add(item);
+        }
+        return symmetricDifference;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return _items.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return ((IEnumerable)_items).GetEnumerator();
+    }
+}
---
+++ SetEx/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace SetEx;
+class Program
+{
+    static void Main(string[] args)
+    {
+        MySet<int> firstBazm = new MySet<int>();
+        MySet<int> secBazm = new MySet<int>();
+
+        firstBazm.Add(2);
+        firstBazm.Add(1);
+        firstBazm.Add(4);
+        firstBazm.Add(22);
+        firstBazm.Add(6);
+        firstBazm.Add(2004);
+
+        secBazm.Add(4);
+        secBazm.Add(2);
+        secBazm.Add(1);
+        secBazm.Add(23);
+        secBazm.Add(8);
+        secBazm.Add(2004);
+
+        var firstResult = firstBazm.Union(secBazm);
+        var secondResult = firstBazm.Intersection(secBazm);
+        var thirdResult = firstBazm.Difference(secBazm);
+        var fourthResult = firstBazm.SymmetricDifference(secBazm);
+
+        Console.WriteLine("Union numbers");
+        foreach (var item in firstResult)
+        {
+            Console.Write(item + ", ");
+        }
+        Console.WriteLine("\nIntersection numbers");
+        foreach (var item in secondResult)
+        {
+            Console.Write(item + ", ");
+        }
+        Console.WriteLine("\nDifference numbers");
+        foreach (var item in thirdResult)
+        {
+            Console.Write(item + ", ");
+        }
+        Console.WriteLine("\nSymmetricDifference numbers");
+        foreach (var item in fourthResult)
+        {
+            Console.Write(item + ", ");
+        }
+    }
+}
